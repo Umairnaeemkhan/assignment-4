7937373575818dea8c1ccf2146a904155bf697ed
Umairnaeemkhan <155988400+Umairnaeemkhan@users.noreply.github.com>
2024-01-25 16:50:00 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+// area of ractangle
+var length = 175;
+var width = 89;
+
+var area = (length*width);
+console.log(area)
+
+
+let userNamed = prompt("Your Name");
+alert("Welcome to my javascript class.")
+
+console.log(userNamed);
